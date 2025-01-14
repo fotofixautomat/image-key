dbc47659a6e61a5e71f7b2980a4bd217c82bec19
Steven Helmis <steven.helmis@scitotec.de>
2025-01-14 16:12:58 +0100

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/image-key',
 	build: {
 		outDir: 'docs', // Ändert den Ausgabeordner auf "docs"
 	},
