84b7eb0864be937d9109e0461ac5ecdeea50ded8
Steven Helmis <steven.helmis@scitotec.de>
2025-01-15 15:50:27 +0100

    add sort items

--- src/components/key-card.jsx
+++ src/components/key-card.jsx
@@ -8,19 +8,22 @@ class KeyCard extends Component {
 		return (
 			<>
 				<div className='masonry sm:masonry-sm md:masonry-md lg:masonry-lg'>
-					{this.state.map((item) => (
-						<div className={`border-solid border-2 m-2 rounded-md ${item.style.border}`}>
-							<h2 className={`font-bold p-1 ${item.style.bgcolor}`}>{item.title}</h2>
-							{item.keys.map((key, index) => {
-								const isLast = index === item.keys.length - 1
-								return (
-									<div className={`px-1 border-solid ${isLast ? '' : 'border-b-2'} ${item.style.border}`}>
-										{key.name}
-									</div>
-								)
-							})}
-						</div>
-					))}
+					{this.state.map((item) => {
+						const sortedKeys = item.keys.sort((a, b) => a.name.localeCompare(b.name))
+						return (
+							<div className={`border-solid border-2 m-2 rounded-md ${item.style.border}`}>
+								<h2 className={`font-bold p-1 ${item.style.bgcolor}`}>{item.title}</h2>
+								{sortedKeys.map((key, index) => {
+									const isLast = index === sortedKeys.length - 1
+									return (
+										<div className={`px-1 border-solid ${isLast ? '' : 'border-b-2'} ${item.style.border}`}>
+											{key.name}
+										</div>
+									)
+								})}
+							</div>
+						)
+					})}
 				</div>
 			</>
 		)
